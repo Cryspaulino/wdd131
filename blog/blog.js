24f644ab7fb96730fceca36fe0377ccb32980511
Cryspaulino <pau22008@byui.edu>
2025-02-19 10:37:24 -0800

    with ta

--- blog/blog.js
+++ blog/blog.js
@@ -25,38 +25,76 @@ const articles = [
 		stars: '⭐⭐⭐⭐'
 	},
 	{
-		id:4,
-		title: '...'
-	}
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his Aunt Pol and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
+		}
 ]
 
 function displayArticles(articles) {
-    const container = document.querySelector("#articles-container");
+	const container = document.querySelector("#articles-container");
 
-    articles.forEach(article => {
-        const newArticle = document.createElement("article");
-        newArticle.classList.add("article-class");
+	// const date = document.querySelector("#leftsect");
+	// const cont = document.querySelector("#fullContainer");
 
-        const articleContent = `
-            <h2>${article.title}</h2>
-			<img src="${article.imgSrc}">
-            <p>${article.description}</p>
+	articles.forEach(article => {
+
+		const fullContainer = document.createElement("div");
+		fullContainer.classList.add("fullContainer")
+
+		const middlesect = document.createElement("section");
+		middlesect.classList.add("article-class");
+		
+		const middleContent = `
+		<h2>${article.title}</h2>
+		<img src="${article.imgSrc}">
+		<p>${article.description}</p>
         `;
-		const date = document.querySelector("#leftsect");
-		const dateContent = `
+		middlesect.innerHTML = middleContent;
+
+		const leftsect = document.createElement("section");
+		leftsect.classList.add("left");
+
+		const leftContent = `
 			<h2>${article.date}</h2>
-			<img src="${article.ages}">
+			<p>${article.ages}</p>
 			<p>${article.genre}</p>
 			<p>${article.stars}</p>
-	
 		`;
 
-        newArticle.innerHTML = articleContent;
-        container.appendChild(newArticle);
-    });
+		leftsect.innerHTML = leftContent
+		
+
+		fullContainer.appendChild(leftsect);
+		fullContainer.appendChild(middlesect);
+		container.appendChild(fullContainer)
+
+
+
+
+
+		// container.appendChild(middlesect);
+		// container.appendChild(leftsect);
+
+
+		// date.appendChild(leftsect);
+
+		// fullContainer.append(container);
+		// fullContainer.append(date);
+		// cont.appendChild(fullContainer);
+	});
 }
 
 displayArticles(articles);
+
 
 
 
